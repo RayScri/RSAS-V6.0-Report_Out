71aea3af4276f41751b7cea4171d07278b718305
RayScri <49472130+RayScri@users.noreply.github.com>
2019-12-06 09:00:07 +0800

    Add files via upload

---
+++ RSAS_Report_http.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author:ray
+# date:2019-12-02
+import re
+import os
+from bs4 import BeautifulSoup
+import sys
+
+if len(sys.argv) != 3:
+	print "Usage: python RSAS_Report_http.py ./DIRname http"
+	sys.exit(1)
+path = sys.argv[1]
+protocols = sys.argv[2]
+
+dirs = os.listdir(path+'/host/')
+for filename in dirs:
+	flag = 'html' in filename
+	if flag == 0:
+		continue
+	f = open(path+'/host/'+filename,'r')
+	html = f.read()
+	soup = BeautifulSoup(html, 'lxml')
+	ip = soup.select('.even > td')[0].text
+	#提取2.1漏洞概况里面的vuln_list表格里的数据
+	#使用id选择器选取含有ID=vuln_list的第一个table标签内所有的内容
+	tables = soup.select('#vuln_list')
+	if len(tables)>0:
+		table = tables[0]		
+	else:
+		continue
+	#print table
+	tbody = table.select('tbody')[0]   #使用select选择器选择table里面的第一个tbody标签【也因为只有一个这个标签】
+	#print tbody
+	trs = tbody.select('tr')  #因为tbody标签下面有好多tr标签，也就是表格，所以选择器选择全部。	
+	for tr in trs:
+		port = tr.select('td')[0].text		
+		protocol = tr.select('td')[2].text
+		if protocols in protocol:
+			print ip+':'+port+':'+protocol
+
+	f.close()
+	
